cbb97c7fafdb28a5b0a43977da440ec1445129cc
LIvanoff <drfrily@gmail.com>
2023-02-15 23:25:28 +0300

    add task13

---
+++ day2/task13/task13.py
@@ -0,0 +1,29 @@
+
+
+def task13():
+    with open("input.txt") as f:
+        s = f.read()
+
+    s = list(s)
+    s = list(filter(lambda sym: sym != ' ' and sym != '\n', s))
+    stack = []
+    for ch in s:
+        if ch != '+' and ch != '-' and ch != '*' and ch != '/':
+            stack.insert(len(stack), ch)
+        elif ch == '+' or ch == '-' or ch == '*' or ch == '/':
+            a: int = stack[len(stack) - 1]
+            stack.pop(len(stack) - 1)
+            b: int = stack[len(stack) - 1]
+            stack.pop(len(stack) - 1)
+            if ch == '*':  # *
+                stack.insert(len(stack), int(b) * int(a))
+            elif ch == '+':  # +
+                stack.insert(len(stack), int(b) + int(a))
+            elif ch == '-':  # -
+                stack.insert(len(stack), int(b) - int(a))
+            elif ch == '/':  # /
+                stack.insert(len(stack), int(b) / int(a))
+    print(stack[0])
+
+
+task13()
