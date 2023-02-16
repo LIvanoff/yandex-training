dd2c10ca312863c8a15191fe9395819d9351b132
Leonid <drfrily@gmail.com>
2023-02-17 02:43:27 +0300

    add task14_v2

---
+++ day2/task14/task14_v2.py
@@ -0,0 +1,31 @@
+
+def task14():
+    with open("input.txt") as f:
+        s = f.read()
+
+    s = list(filter(lambda sym: sym != ' ' and sym != '\n', s))
+    first_railway = list(map(int, s))
+    first_railway.pop(0)
+    dead_stack = []
+    second_railway = []
+
+    while len(first_railway) > 0:
+        tmp = first_railway[0]
+        first_railway.pop(0)
+        while len(dead_stack) > 0 and dead_stack[0] < tmp:
+            second_railway.insert(len(second_railway), dead_stack[0])
+            dead_stack.pop(0)
+        dead_stack.insert(0, tmp)
+
+    while len(dead_stack) > 0:
+        if len(second_railway) !=0:
+            if dead_stack[0] < second_railway[-1]:
+                print('NO')
+                break
+        second_railway.insert(len(second_railway), dead_stack[0])
+        dead_stack.pop(0)
+        if len(dead_stack) == 0:
+            print('YES')
+
+
+task14()
