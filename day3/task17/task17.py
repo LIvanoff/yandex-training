7e81b9fcc766f86553a1c21e1861e9177977b86e
LIvanoff <drfrily@gmail.com>
2023-02-17 23:57:43 +0300

    add files

---
+++ day3/task17/task17.py
@@ -0,0 +1,44 @@
+def check_par(card1, card2):
+    if card1 == 0 and card2 == 9:
+        return 9, 0
+    elif card1 == 9 and card2 == 0:
+        return 0, 9
+    else:
+        return card1, card2
+
+
+def task17():
+    with open("input.txt") as f:
+        first = f.readline()
+        second = f.readline()
+
+    first = list(map(int, first.split(' ')))
+    second = list(map(int, second.split(' ')))
+
+    count = 1
+    while True:
+        card_player1 = first[0]
+        card_player2 = second[0]
+        card_player1, card_player2 = check_par(card_player1, card_player2)
+
+        if card_player1 > card_player2:
+            first.append(first.pop(0))
+            first.append(second.pop(0))
+
+        elif card_player1 < card_player2:
+            second.append(second.pop(0))
+            second.append(first.pop(0))
+
+        if len(second) < 1:
+            print('first ' + str(count))
+            break
+        elif len(first) < 1:
+            print('second ' + str(count))
+            break
+        if count == 1000000:
+            print('botva')
+            break
+        count += 1
+
+
+task17()
---
+++ day3/task18/script.py
@@ -0,0 +1,7 @@
+a = 10
+
+
+if a > 2:
+    print('A')
+elif a > 3:
+    print('B')
