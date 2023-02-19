0271e7e635a2290e881e53beb891190b8b49a412
Leonid <drfrily@gmail.com>
2023-02-19 17:18:55 +0300

    update task19

--- day3/task19/task19.py
+++ day3/task19/task19.py
@@ -1,10 +1,12 @@
-
 class Heap(object):
     def __init__(self):
         self.heap = []
 
     def sort(self):
-        pass
+        pos = len(self.heap) - 1
+        while pos > 0 and self.heap[pos] > self.heap[(pos - 1) // 2]:
+            self.heap[pos], self.heap[(pos - 1) // 2] = self.heap[pos - 1] // 2, self.heap[pos]
+            pos = (pos - 1) // 2
 
     def insert(self, num):
         self.heap.append(num)
@@ -15,12 +17,15 @@ class Heap(object):
 
 
 def task19():
+    hp = Heap()
     num_command = int(input())
     for i in range(num_command):
         command = input().split()
         if command[0] == '0':
             num = int(command[1])
+            hp.insert(num)
             print(num)
+            print(hp.heap)
         else:
             pass
 
